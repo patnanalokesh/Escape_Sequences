66b6c6f627b220d890aa1055b0f0d95b0ebc5914
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-08-15 16:11:43 +0530

    Add files via upload

---
+++ Escape_Sequences.py
@@ -0,0 +1,81 @@
+# print("hello\arun")
+# print("hello\aarun")
+# print("hello\\arun")
+# print("hello\badhra") # Last element will be eleminated. 
+# print("hello \badhra")
+# print("hello\cat")
+# print("hello\dog")
+# print("hello\fox")  #nextline
+# print("hello\frog")  #nextline
+# print("hello\goat")
+# print("hello\hut")
+# print("hello\ice")
+# print("hello\jug")
+# print("hello\king")
+# print("hello\lion")
+# print("hello\moon")
+# print("hello\name")  # \n --->nextline
+# print("hello\one")
+# print("hello\para")
+# print("hello\queen")
+# print("hello\rose") #right side of r how many elements are have remaining elements of left side are attaching except nuber of right elements.
+# print("hello\son")
+# print("hello\topper")
+# print("\tournament")
+# print("hello\union")  #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes
+# print("hello\\union")
+# print("hello\version")  # newline
+# print("hello\world")
+# print("hello\xerox") #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes
+# print("hello\\xerox") 
+# print("hello\young")
+# print("hellp\zebra")
+
+# print("HELLO\ANT")
+# print("HELLO\NOUNE")  #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes
+# print("HELLO\\NOUNE")  
+# print("HELLO\UNIVERSE")  #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes
+# print("HELLO\\UNIVERSE")
+
+# print("hello\nworld")
+# print('hello pyth\'on')
+# print("hello py\"thon")
+# print('F:\Uatch_9_15_AM') #SyntaxError: (unicode error) 'unicodeescape' codec can't decode bytes
+# print('F:\\Uatch_9_15_AM')
+# print('L:\\User\\LOKESH_HD') 
+# print('L:\\User\\LOKI\\DOWNLOADS')
+# print('\u0001☺')
+# print('\u0002')
+# print('\u0003♥')
+# print('\u0004')
+# print('\u0005')
+# print('\u0006♠')
+# print('\u0007')
+# print('\u0008')
+# print('\u0009')
+# print('\U0001f601😁')
+# print('\U0001f602')
+# print('\U0001f603')
+# print('\U0001f604')
+# print('\U0001f605')
+
+import emoji  # ModuleNotFoundError: No module named 'emoji'
+# print(emoji.emojize(":grinning_face_with_big_eyes:"))
+# print(emoji.emojize(":winking_face_with_tongue:"))
+# print(emoji.emojize(":zipper-mouth_face:"))
+# print(emoji.demojize("😃"))
+# print(emoji.demojize("🤐"))
+# print(emoji.demojize("😜"))
+
+# print("C:\\Users\\PATNANA LOKESWARARAO\\OneDrive\\Documents")
+# print("C:\\Users\PATNANA LOKESWARARAO\OneDrive\Documents")
+
+# print("hello \bworld")
+# print("py\"thon")
+# print("py\ron")
+# print("pyth\ron")
+# print("pythpy\ron")
+# print("hello\rpy ")
+
+# print("My Name is \r Allu Arjun")
+# print("My Name is \r Lokesh")
